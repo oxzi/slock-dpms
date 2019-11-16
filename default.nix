a3d409e5c61bce14aedb6c3e4830321ec2f27b27
geistesk <github.jyf300hj@0x21.biz>
2019-11-16 20:35:12 +0100

    Update default.nix for current nixpkgs

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@ let
 in rec {
   slock = stdenv.mkDerivation rec {
     name = "slock-1.4";
-    src = ./.;
-    buildInputs = with pkgs.xorg; [ xproto libX11 libXext libXrandr ];
+    src = stdenv.lib.cleanSource ./.;
+    buildInputs = with pkgs.xorg; [ xorgproto libX11 libXext libXrandr ];
     installFlags = "DESTDIR=\${out} PREFIX=";
     patchPhase = "sed -i '/chmod u+s/d' Makefile";
   };
